4ff15277871027754c3ce36bde02b326de0ac259
shein <msh86.02.18@rambler.ru>
2021-01-29 14:23:02 +0500

    Добавил расчет прочентали

--- src/main/java/shein/dmitriy/main/Main.java
+++ src/main/java/shein/dmitriy/main/Main.java
@@ -7,24 +7,25 @@ import org.json.simple.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String fileName = "tickets.json";
+        int average = 0;
 
         String tmpData = new String(Files.readAllBytes(Paths.get(fileName)));
 
         Gson gson = new Gson();
         JSONObject tmpJsonObject = gson.fromJson(tmpData, JSONObject.class);
 
-        ArrayList<Integer> minOnWay = new ArrayList<>();
+        ArrayList<Integer> minOnWayArray = new ArrayList<>();
+
 
         for (Object o: (ArrayList) tmpJsonObject.get("tickets")) {
             JsonObject tmpTicket = gson.toJsonTree(o).getAsJsonObject();
@@ -38,9 +39,19 @@ public class Main {
             String[] departureTime = ticket.getDepartureTime().split(":");
             String[] arrivalTime = ticket.getArrivalTime().split(":");
 
-            minOnWay.add(60 * 24 * period.getDays() - 60 * Integer.parseInt(departureTime[0]) - Integer.parseInt(departureTime[1])
-            + 60 * Integer.parseInt(arrivalTime[0]) + Integer.parseInt(arrivalTime[1]));
-            System.out.println(minOnWay);
+            int minOnWay =  60 * 24 * period.getDays() - 60 * Integer.parseInt(departureTime[0]) - Integer.parseInt(departureTime[1])
+                    + 60 * Integer.parseInt(arrivalTime[0]) + Integer.parseInt(arrivalTime[1]);
+            minOnWayArray.add(minOnWay);
+            average += minOnWay;
+            System.out.println(minOnWayArray);
         }
+        average = average / minOnWayArray.size();
+        System.out.println("Среднее время в пути: " + average / 60 + ":" + average % 60);
+        Collections.sort(minOnWayArray);
+        System.out.println(minOnWayArray);
+        int indexPercentiles = minOnWayArray.size() * 90 / 100;
+        System.out.println( minOnWayArray.get(indexPercentiles - 1));
+
+
     }
 }
